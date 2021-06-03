4fb40e79e61130a7d8adff449777ee8037ac23ad
daze_77 <daze77@gmail.com>
2021-06-03 19:37:30 -0400

    code refactoring on all pages.  Removing code no longer needed based on old sidebar

--- script/script.js
+++ script/script.js
@@ -1,10 +1,4 @@
 
    
-     $(document).ready(function () {
-         $('#sidebarCollapse').on('click', function () {
-             $('#sidebar').toggleClass('active');
-             $(this).toggleClass('active');
-         });
-     });
 
 
